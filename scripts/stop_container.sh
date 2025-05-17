753dd175e8f100b3df34bdf3f9d2026f5e0eae4a
rubesh-git <rubesh.redington@gmail.com>
2025-05-17 18:18:01 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Stop the running container (if any)
-containerid=`docker ps -q | awk -F " " '{print $1}'`
-docker rm -f $containerid
+# Stop and remove the running container (if any)
+containerid=$(docker ps -q)
 
-####################################################
+if [ -n "$containerid" ]; then
+    echo "Stopping container: $containerid"
+    docker rm -f $containerid
+else
+    echo "No running container to stop."
+fi
